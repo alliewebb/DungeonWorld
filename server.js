dddf2c222e974e799b5652ced9631b1a856cd1f5
Allie Webb <alliewebb@Allies-MacBook-Pro.local>
2018-11-03 20:29:07 +1300

    Set up complete

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const userRoutes = require('./routes/routes')
+
+const server = express()
+
+// Middleware
+
+server.engine('hbs', hbs({extname: 'hbs'}))
+server.set('view engine', 'hbs')
+server.use(express.urlencoded({extended: true}))
+server.use(express.static('Public'))
+
+// Routes
+
+server.use('/', userRoutes)
+
+module.exports = server
